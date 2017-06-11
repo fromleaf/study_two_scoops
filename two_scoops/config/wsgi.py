95b589515a0f64aa034d449b905e2f053303db57
yoon <fromleaf@gmail.com>
2017-06-11 14:49:54 +0900

    modify file and folder's structure

--- two_scoops/config/wsgi.py
+++ two_scoops/config/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "two_scoops.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "two_scoops.config.settings.settings"
+)
 
 application = get_wsgi_application()
